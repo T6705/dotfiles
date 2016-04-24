e96d482688fce6bc3688862d7a7feebb2ac6eeb7
T6705 <chris1128999@gmail.com>
2016-04-24 19:05:42 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -37,12 +37,16 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vim/colors/molokai.vim --create-dirs \
     https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+curl -fLo ~/.vim/plugin/dragvisuals.vim --create-dirs \
+    https://raw.githubusercontent.com/T6705/dotfile/master/.vim/plugin/dragvisuals.vim
 
 ### nvim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.config/nvim/colors/molokai.vim --create-dirs \
-	https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+    https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+curl -fLo ~/.config/nvim/plugin/dragvisuals.vim --create-dirs \
+    https://raw.githubusercontent.com/T6705/dotfile/master/.config/nvim/plugin/dragvisuals.vim
 
 echo "=============================="
 echo "== Download vim/nvim config =="
@@ -58,9 +62,12 @@ echo "===================="
 echo "== Update Plugins =="
 echo "===================="
 
+curl https://raw.githubusercontent.com/T6705/dotfile/master/visincr.vba > /tmp/visincr.vba
 sudo npm -g install instant-markdown-d
 
+vim /tmp/visincr.vba +"so %" +qall
 vim +PlugUpdate 
+nvim /tmp/visincr.vba +"so %" +qall
 nvim +PlugUpdate 
 reset
 
