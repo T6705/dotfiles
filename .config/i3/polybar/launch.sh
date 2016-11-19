01b65f2221c5ec91ef60c8138466fbf236a9243a
tom <chris1128999@gmail.com>
2016-11-19 16:51:48 +0800

    2016 snapshot

--- .config/i3/polybar/launch.sh
+++ .config/i3/polybar/launch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall polybar
+killall lemonbuddy
 
 # Wait until the processes have been shut down
-#while pgrep -x polybar >/dev/null; do sleep 1; done
+#while pgrep -x lemonbuddy >/dev/null; do sleep 1; done
 
 # Launch bars
-polybar top_bar &
-polybar bottom_bar &
+lemonbuddy top_bar -c ~/.config/i3/polybar/config &
+lemonbuddy bottom_bar -c ~/.config/i3/polybar/config &
