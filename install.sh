7fecfbc7ba20384cfb056e06e4abab4a1ab0e6f8
tom <chris1128999@gmail.com>
2017-06-12 03:09:32 +0800

    2017 snapshot

--- install.sh
+++ install.sh
@@ -1,53 +1,97 @@
 #!/bin/bash
 
-function install_zsh {
+function install_i3 {
     if which apt-get &> /dev/null ; then
-        sudo apt-get install -y zsh
+        sudo apt-get install -y i3
     elif which pacman &> /dev/null ; then
-        sudo pacman -S --noconfirm zsh
+        sudo pacman -S --noconfirm i3-wm
     fi
 
-    echo "========================="
-    echo "== Download zsh config =="
-    echo "========================="
+    echo "========================"
+    echo "== Download i3 config =="
+    echo "========================"
 
-    curl https://raw.githubusercontent.com/T6705/dotfile/master/.zshrc > ~/.zshrc
+    curl -fLo ~/.config/i3/config --create-dirs \
+        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/config
+    curl -fLo ~/.config/i3/i3blocks/i3blocks.conf --create-dirs \
+        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/i3blocks/i3blocks.conf
+    curl -fLo ~/.config/i3/compton.conf --create-dirs \
+        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/compton.conf
+    curl -fLo ~/.config/i3/polybar/config --create-dirs \
+        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/polybar/config
+    curl -fLo ~/.config/i3/polybar/launch.sh --create-dirs \
+        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/polybar/launch.sh
 
-    echo "====================================="
-    echo "== Download zsh plugins and themes =="
-    echo "====================================="
+    echo "======================"
+    echo "== Download i3-gaps =="
+    echo "======================"
 
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    mkdir -p ~/git
+    cd ~/git
+    rm -rf i3-gaps
 
-    mkdir ~/.oh-my-zsh/custom/plugins
-    git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-    #git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-    #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    # clone the repository
+    git clone https://www.github.com/Airblader/i3 i3-gaps
+    cd i3-gaps
 
-    mkdir ~/.oh-my-zsh/custom/themes
-    curl "https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh" > ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+    # compile & install
+    autoreconf --force --install
+    rm -rf build/
+    mkdir -p build && cd build/
 
-    echo "======================="
-    echo "== install powerline =="
-    echo "======================="
+    # Disabling sanitizers is important for release versions!
+    # The prefix and sysconfdir are, obviously, dependent on the distribution.
+    ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+    make
+    sudo make install
 
+    echo "==========================="
+    echo "== Download i3lock-fancy =="
+    echo "==========================="
+
+    mkdir -p ~/git
+    cd ~/git
+    git clone "https://github.com/meskarune/i3lock-fancy"
+    cd ~/git/i3lock-fancy
+    sudo cp lock /usr/local/bin/
+    sudo cp -r icons /usr/local/bin/
+}
+
+function install_ranger {
     if which apt-get &> /dev/null ; then
-        sudo apt-get install fonts-powerline -y
+        sudo apt-get install -y ranger
     elif which pacman &> /dev/null ; then
-        sudo pacman -S --noconfirm powerline
+        sudo pacman -S --noconfirm ranger
     fi
 
-    cd ~
-    git clone https://github.com/milkbikis/powerline-shell
-    python ~/powerline-shell/install.py
-    ln -s ~/powerline-shell/powerline-shell.py ~/powerline-shell.py
+    echo "============================"
+    echo "== Download ranger config =="
+    echo "============================"
+
+    curl -fLo ~/.config/ranger/rc.conf --create-dirs \
+        https://raw.githubusercontent.com/T6705/dotfile/master/.config/ranger/rc.conf
+    curl -fLo ~/.config/ranger/rifle.conf --create-dirs \
+        https://raw.githubusercontent.com/T6705/dotfile/master/.config/ranger/rifle.conf
+}
+
+function install_spacemacs {
+    if which apt-get &> /dev/null ; then
+        sudo apt-get install -y emacs
+    elif which pacman &> /dev/null ; then
+        sudo pacman -S --noconfirm emacs
+    fi
 
     echo "======================="
-    echo "== install LS_COLORS =="
+    echo "== install spacemacs =="
     echo "======================="
-    # https://github.com/trapd00r/LS_COLORS
-    wget https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS -O $HOME/.dircolors
+
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+    echo "==========================="
+    echo "== Download emacs config =="
+    echo "==========================="
+
+    curl https://raw.githubusercontent.com/T6705/dotfile/master/.spacemacs > ~/.spacemacs
 }
 
 function install_tmux {
@@ -138,106 +182,82 @@ function install_vim {
     reset
 }
 
-function install_spacemacs {
+function install_zsh {
     if which apt-get &> /dev/null ; then
-        sudo apt-get install -y emacs
+        sudo apt-get install -y zsh
     elif which pacman &> /dev/null ; then
-        sudo pacman -S --noconfirm emacs
+        sudo pacman -S --noconfirm zsh
     fi
 
+    echo "========================="
+    echo "== Download zsh config =="
+    echo "========================="
+
+    curl https://raw.githubusercontent.com/T6705/dotfile/master/.zshrc > ~/.zshrc
+
+    echo "====================================="
+    echo "== Download zsh plugins and themes =="
+    echo "====================================="
+
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+    mkdir ~/.oh-my-zsh/custom/plugins
+    git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    #git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+    mkdir ~/.oh-my-zsh/custom/themes
+    curl "https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh" > ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+
     echo "======================="
-    echo "== install spacemacs =="
+    echo "== install powerline =="
     echo "======================="
 
-    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
-    echo "==========================="
-    echo "== Download emacs config =="
-    echo "==========================="
-
-    curl https://raw.githubusercontent.com/T6705/dotfile/master/.spacemacs > ~/.spacemacs
-}
-
-function install_i3 {
     if which apt-get &> /dev/null ; then
-        sudo apt-get install -y i3
+        sudo apt-get install fonts-powerline -y
     elif which pacman &> /dev/null ; then
-        sudo pacman -S --noconfirm i3-wm
+        sudo pacman -S --noconfirm powerline
     fi
 
-    echo "========================"
-    echo "== Download i3 config =="
-    echo "========================"
+    cd ~
+    git clone https://github.com/milkbikis/powerline-shell
+    python ~/powerline-shell/install.py
+    ln -s ~/powerline-shell/powerline-shell.py ~/powerline-shell.py
 
-    curl -fLo ~/.config/i3/config --create-dirs \
-        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/config
-    curl -fLo ~/.config/i3/i3blocks/i3blocks.conf --create-dirs \
-        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/i3blocks/i3blocks.conf
-    curl -fLo ~/.config/i3/compton.conf --create-dirs \
-        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/compton.conf
-    curl -fLo ~/.config/i3/polybar/config --create-dirs \
-        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/polybar/config
-    curl -fLo ~/.config/i3/polybar/launch.sh --create-dirs \
-        https://raw.githubusercontent.com/T6705/dotfile/master/.config/i3/polybar/launch.sh
-
-    echo "======================"
-    echo "== Download i3-gaps =="
-    echo "======================"
-
-    mkdir -p ~/git
-    cd ~/git
-    rm -rf i3-gaps
-
-    # clone the repository
-    git clone https://www.github.com/Airblader/i3 i3-gaps
-    cd i3-gaps
-
-    # compile & install
-    autoreconf --force --install
-    rm -rf build/
-    mkdir -p build && cd build/
-
-    # Disabling sanitizers is important for release versions!
-    # The prefix and sysconfdir are, obviously, dependent on the distribution.
-    ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-    make
-    sudo make install
-
-    echo "==========================="
-    echo "== Download i3lock-fancy =="
-    echo "==========================="
-
-    mkdir -p ~/git
-    cd ~/git
-    git clone "https://github.com/meskarune/i3lock-fancy"
-    cd ~/git/i3lock-fancy
-    sudo cp lock /usr/local/bin/
-    sudo cp -r icons /usr/local/bin/
+    echo "======================="
+    echo "== install LS_COLORS =="
+    echo "======================="
+    # https://github.com/trapd00r/LS_COLORS
+    wget https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS -O $HOME/.dircolors
 }
 
 if which apt-get &> /dev/null ; then
-    sudo apt-get install zsh tmux xclip xsel npm vim vim-athena vim-gnome vim-gtk vim-nox -y
+    sudo apt-get install zsh ranger tmux xclip xsel npm vim vim-athena vim-gnome vim-gtk vim-nox -y
 elif which pacman &> /dev/null ; then
-    sudo pacman -Sy --noconfirm zsh tmux xclip xsel npm vim
+    sudo pacman -Sy --noconfirm zsh ranger tmux xclip xsel npm vim
 fi
 sudo npm install npm@latest -g
 
 ans=$1
 
-if [ $ans == "zsh" ]; then
-    install_zsh
+if [ $ans == "i3" ]; then
+    install_i3
+elif [ $ans == "ranger" ]; then
+    install_ranger
+elif [ $ans == "spacemacs" ]; then
+    install_spacemacs
 elif [ $ans == "tmux" ]; then
     install_tmux
 elif [ $ans == "vim" ]; then
     install_vim
-elif [ $ans == "spacemacs" ]; then
-    install_spacemacs
-elif [ $ans == "i3" ]; then
-    install_i3
-else
+elif [ $ans == "zsh" ]; then
     install_zsh
+else
+    install_i3
+    install_ranger
+    install_spacemacs
     install_tmux
     install_vim
-    install_spacemacs
-    install_i3
+    install_zsh
 fi
