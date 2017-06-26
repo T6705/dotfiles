aa524bb6f8070fd278b213b79fc9d5f241cf80e1
tom <chris1128999@gmail.com>
2017-06-26 21:49:58 +0800

    2017 snapshot

--- .config/nvim/mappings.vim
+++ .config/nvim/mappings.vim
@@ -125,9 +125,11 @@ nnoremap <silent> <F4> :GundoToggle<CR>
 nnoremap <silent> <F5> :Codi!!<CR>
 
 " hexedit
-inoremap <silent> <F7> <esc>:%!xxd<CR>
+"inoremap <silent> <F7> <esc>:%!xxd<CR>
+"noremap <silent> <F7> :%!xxd<CR>
+inoremap <silent> <F7> <esc>:Hexmode<CR>
 inoremap <silent> <F8> <esc>:%!xxd -r<CR>
-noremap <silent> <F7> :%!xxd<CR>
+noremap <silent> <F7> :Hexmode<CR>
 noremap <silent> <F8> :%!xxd -r<CR>
 
 " qq to record, Q to replay (recursive noremap due to peekaboo)
--- install.sh
+++ install.sh
@@ -206,6 +206,7 @@ function install_zsh {
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
     mkdir -p ~/.oh-my-zsh/custom/plugins
+    git clone https://github.com/djui/alias-tips.git ~/.oh-my-zsh/custom/plugins/alias-tips
     git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
     #git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
