28f6a3fa148581fc862259468eec420f4b875be3
tom <chris1128999@gmail.com>
2017-08-07 06:39:08 +0800

    2017 snapshot

--- MAGOON.sh
+++ MAGOON.sh
@@ -1,3 +1,5 @@
+#dconf reset -f /org/gnome/terminal/legacy/profiles:/
+
 ####################################################
 ## source: https://github.com/NorthernTwig/Magoon ##
 ####################################################
@@ -5,31 +7,68 @@
 #get profileid and display the value of each key
 profileid=`dconf list /org/gnome/terminal/legacy/profiles:/`
 path="/org/gnome/terminal/legacy/profiles:/$profileid"
-echo "\nprofileid = $profileid"
-echo "path      = $path\n"
+echo "profileid = $profileid"
+echo "path      = $path"
 for i in `eval dconf list $path`; do
+    echo "==========================================================="
+    echo ""
     value=$path$i
     echo "$i:"
     eval dconf read $value
     echo ""
 done
 
-#set terminal colorscheme
+#################
+## colorscheme ##
+#################
 
-#foreground-color
 foreground_color="'rgb(197,200,198)'"
 foreground_path=$path"foreground-color"
-#echo "dconf write $foreground_path $foreground_color"
 eval dconf write $foreground_path \"$foreground_color\"
 
-#background-color
 background_color="'rgb(35,47,57)'"
 background_path=$path"background-color"
-#echo "dconf write $background_path $background_color"
 eval dconf write $background_path \"$background_color\"
 
-#palette
 palette_color="['rgb(35,47,57)', 'rgb(95,171,217)', 'rgb(109,198,228)', 'rgb(157,168,171)', 'rgb(161,209,157)', 'rgb(179,182,195)', 'rgb(161,210,224)', 'rgb(227,233,232)', 'rgb(109,198,228)', 'rgb(95,171,217)', 'rgb(109,198,228)', 'rgb(157,168,171)', 'rgb(161,209,157)', 'rgb(179,182,195)', 'rgb(161,210,224)', 'rgb(227,233,232)']"
 palette_path=$path"palette"
-#echo "dconf write $palette_path $palette_color"
 eval dconf write $palette_path \"$palette_color\"
+
+#############
+## setting ##
+#############
+scroll_on_output_value="true"
+scroll_on_output_path=$path"scroll-on-output"
+eval dconf write $scroll_on_output_path \"$scroll_on_output_value\"
+
+scroll_on_keystroke_value="false"
+scroll_on_keystroke_path=$path"scroll-on-keystroke"
+eval dconf write $scroll_on_keystroke_path \"$scroll_on_keystroke_value\"
+
+scroll_policy_value="'never'"
+scroll_policy_path=$path"scroll-policy"
+eval dconf write $scroll_policy_path \"$scroll_policy_value\"
+
+scrollbar_policy_value="'never'"
+scrollbar_policy_path=$path"scrollbar-policy"
+eval dconf write $scrollbar_policy_path \"$scrollbar_policy_value\"
+
+use_transparent_background_value="false"
+use_transparent_background_path=$path"use-transparent-background"
+eval dconf write $use_transparent_background_path \"$use_transparent_background_value\"
+
+use_theme_colors_value="false"
+use_theme_colors_path=$path"use-theme-colors"
+eval dconf write $use_theme_colors_path \"$use_theme_colors_value\"
+
+use_system_colors_value="false"
+use_system_colors_path=$path"use-system-colors"
+eval dconf write $use_system_colors_path \"$use_system_colors_value\"
+
+use_system_font_value="false"
+use_system_font_path=$path"use-system-font"
+eval dconf write $use_system_font_path \"$use_system_font_value\"
+
+font_value="'Knack Nerd Font 11'"
+font_path=$path"font"
+eval dconf write $font_path \"$font_value\"
--- nord.sh
+++ nord.sh
@@ -1,27 +1,27 @@
 #dconf reset -f /org/gnome/terminal/legacy/profiles:/
 
-############################################
-### setup colorscheme for gnome-terminal ###
-############################################
+####################################################################
+## source: https://github.com/arcticicestudio/nord-gnome-terminal ##
+####################################################################
 
-terminalbg="#1d2021" #rgb(29,32,33)
-terminalfg="#fbf1c7" #rgb(251,241,199)
-color0="#282828"     #rgb(40,40,40)
-color1="#9d0006"     #rgb(157,0,6)
-color2="#79740e"     #rgb(121,116,14)
-color3="#b57614"     #rgb(181,118,20)
-color4="#076678"     #rgb(7,102,120)
-color5="#8f3f71"     #rgb(143,63,113)
-color6="#427b58"     #rgb(66,123,88)
-color7="#7c6f64"     #rgb(124,111,100)
-color8="#928374"     #rgb(146,131,116)
-color9="#cc241d"     #rgb(204,36,29)
-color10="#98971a"    #rgb(152,151,26)
-color11="#d79921"    #rgb(215,153,33)
-color12="#458588"    #rgb(69,133,136)
-color13="#b16286"    #rgb(177,98,134)
-color14="#8ec07c"    #rgb(142,192,124)
-color15="#ebdbb2"    #rgb(235,219,178)
+terminalbg="#2E3440" #rgb(46,52,64)
+terminalfg="#D8DEE9" #rgb(216,222,233)
+color0="#3B4252"     #rgb(59,66,82)
+color1="#BF616A"     #rgb(191,97,106)
+color2="#A3BE8C"     #rgb(163,190,140)
+color3="#EBCB8B"     #rgb(235,203,139)
+color4="#81A1C1"     #rgb(129,161,193)
+color5="#B48EAD"     #rgb(180,142,173)
+color6="#88C0D0"     #rgb(136,192,208)
+color7="#E5E9F0"     #rgb(229,233,240)
+color8="#4C566A"     #rgb(76,86,106)
+color9="#BF616A"     #rgb(191,97,106)
+color10="#A3BE8C"    #rgb(163,190,140)
+color11="#EBCB8B"    #rgb(235,203,139)
+color12="#81A1C1"    #rgb(129,161,193)
+color13="#B48EAD"    #rgb(180,142,173)
+color14="#8FBCBB"    #rgb(143,188,187)
+color15="#ECEFF4"    #rgb(236,239,244)
 
 #get profileid and display the value of each key
 profileid=`dconf list /org/gnome/terminal/legacy/profiles:/`
@@ -41,15 +41,15 @@ done
 ## colorscheme ##
 #################
 
-foreground_color="'rgb(251,241,199)'"
+foreground_color="'rgb(216,222,233)'"
 foreground_path=$path"foreground-color"
 eval dconf write $foreground_path \"$foreground_color\"
 
-background_color="'rgb(29,32,33)'"
+background_color="'rgb(46,52,64)'"
 background_path=$path"background-color"
 eval dconf write $background_path \"$background_color\"
 
-palette_color="['rgb(40,40,40)', 'rgb(157,0,6)', 'rgb(121,116,14)', 'rgb(181,118,20)', 'rgb(7,102,120)', 'rgb(143,63,113)', 'rgb(66,123,88)', 'rgb(124,111,100)', 'rgb(146,131,116)', 'rgb(204,36,29)', 'rgb(152,151,26)', 'rgb(215,153,33)', 'rgb(69,133,136)', 'rgb(177,98,134)', 'rgb(142,192,124)', 'rgb(235,219,178)']"
+palette_color="['rgb(59,66,82)', 'rgb(191,97,106)', 'rgb(163,190,140)', 'rgb(235,203,139)', 'rgb(129,161,193)', 'rgb(180,142,173)', 'rgb(136,192,208)', 'rgb(229,233,240)', 'rgb(76,86,106)', 'rgb(191,97,106)', 'rgb(163,190,140)', 'rgb(235,203,139)', 'rgb(129,161,193)', 'rgb(180,142,173)', 'rgb(143,188,187)', 'rgb(236,239,244)']"
 palette_path=$path"palette"
 eval dconf write $palette_path \"$palette_color\"
 
