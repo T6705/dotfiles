d83f747586ffa672046c7bfe2f81c5718a2ad4f0
T6705 <chris1128999@gmail.com>
2022-05-18 01:21:42 +0800

    2022 snapshot

--- .config/nvim/lua/config/vim-fugitive.lua
+++ .config/nvim/lua/config/vim-fugitive.lua
@@ -1,11 +1,11 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<leader>gb', ':Gblame<CR>', opts)
-map('n', '<leader>gc', ':Gcommit<CR>', opts)
+map('n', '<leader>gb', ':Git blame<CR>', opts)
+map('n', '<leader>gc', ':Git commit<CR>', opts)
 map('n', '<leader>gd', ':Gvdiff<CR>', opts)
-map('n', '<leader>gps', ':Gpush<CR>', opts)
-map('n', '<leader>gpu', ':Gpull<CR>', opts)
-map('n', '<leader>gr', ':Gremove<CR>', opts)
-map('n', '<leader>gs', ':Gstatus<CR>', opts)
+map('n', '<leader>gps', ':Git push<CR>', opts)
+map('n', '<leader>gpu', ':Git pull<CR>', opts)
+map('n', '<leader>gr', ':GRemove<CR>', opts)
+map('n', '<leader>gs', ':Git<CR>', opts)
 map('n', '<leader>gw', ':Gwrite<CR>', opts)
