4dcf7ba16e8b8afa2171b39ad2fd6c19f5524fe5
T6705 <chris1128999@gmail.com>
2022-06-27 06:32:48 +0800

    change the color of sign of breakpoint

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -420,7 +420,38 @@ return require('packer').startup({ function(use)
         run = "git clone https://github.com/go-delve/delve /tmp/delve && cd /tmp/delve && go install github.com/go-delve/delve/cmd/dlv && rm -rf /tmp/delve",
         config = function() require('dap-go').setup() end
       }
-    }
+    },
+    config = function()
+      vim.highlight.create('DapBreakpoint', { ctermbg = 0, guifg = '#993939', guibg = '#31353f' }, false)
+      vim.highlight.create('DapLogPoint', { ctermbg = 0, guifg = '#61afef', guibg = '#31353f' }, false)
+      vim.highlight.create('DapStopped', { ctermbg = 0, guifg = '#98c379', guibg = '#31353f' }, false)
+
+      vim.fn.sign_define('DapBreakpoint', {
+        text = '',
+        texthl = 'DapBreakpoint',
+        linehl = 'DapBreakpoint',
+        numhl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointCondition', {
+        text = 'ﳁ',
+        texthl = 'DapBreakpoint',
+        linehl = 'DapBreakpoint',
+        numhl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointRejected', {
+        text = '',
+        texthl = 'DapBreakpoint',
+        linehl = 'DapBreakpoint',
+        numhl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapLogPoint', {
+        text = '',
+        texthl = 'DapLogPoint',
+        linehl = 'DapLogPoint',
+        numhl = 'DapLogPoint' })
+      vim.fn.sign_define('DapStopped', {
+        text = '',
+        texthl = 'DapStopped',
+        linehl = 'DapStopped',
+        numhl = 'DapStopped' })
+    end
   }
 
   --------------------------------------------------------------------------------
