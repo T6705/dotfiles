87a2ec321432256c9d509323ee7f568369285caa
T6705 <chris1128999@gmail.com>
2023-05-17 12:34:28 +0800

    lsp-zero.nvim: use branch 'v2.x'

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -351,11 +351,12 @@ require("lazy").setup({
   --------------------------------------------------------------------------------
   {
     'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
     dependencies = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+      { 'neovim/nvim-lspconfig' },                                      -- Required
+      { 'williamboman/mason.nvim',            build = ":MasonUpdate" }, -- Optional
+      { 'williamboman/mason-lspconfig.nvim' },                          -- Optional
 
       -- Autocompletion
       { 'hrsh7th/nvim-cmp' },                    -- Required
