81bd5efea484a0ca541ab612c55069027dacac7f
T6705 <chris1128999@gmail.com>
2023-08-19 23:53:44 +0800

    update gitsigns mapping

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -272,13 +272,23 @@ require("lazy").setup({
     config = function()
       require('gitsigns').setup({
         current_line_blame = true,
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-          delay = 250,
-          ignore_whitespace = false,
-        },
-        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+        on_attach = function(buffer)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+          end
+
+          map("n", "]h", gs.next_hunk, "Next Hunk")
+          map("n", "[h", gs.prev_hunk, "Prev Hunk")
+          map({ "n", "v" }, "<leader>sh", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+          map({ "n", "v" }, "<leader>rh", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+          map("n", "<leader>sb", gs.stage_buffer, "Stage Buffer")
+          map("n", "<leader>ush", gs.undo_stage_hunk, "Undo Stage Hunk")
+          map("n", "<leader>rb", gs.reset_buffer, "Reset Buffer")
+          map("n", "<leader>ph", gs.preview_hunk, "Preview Hunk")
+          map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+        end,
       })
     end
   },
